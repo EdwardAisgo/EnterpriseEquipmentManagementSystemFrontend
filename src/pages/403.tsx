fdd54fed754d01331f6509c74c9a7a6a333f0fcd
chihua <chihua@zenki.ai>
2026-04-17 19:24:06 +0800

    feat(report): update

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -7,7 +7,7 @@ const ForbiddenPage: React.FC = () => (
     <Result
       status="403"
       title="403"
-      subTitle="抱歉，您没有权限访问该页面。"
+      subTitle="抱歉，您没有权限访问该页面！"
       extra={
         <Button type="primary" onClick={() => history.push('/')}>
           返回首页
